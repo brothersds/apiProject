557d141bcffcd687bb2325a1f69d3ba59e5823c7
Dmitry <brothersds@ngs.ru>
2022-12-07 20:19:52 +0300

    begin lesson_7 Check_json_token

--- utils/checking.py
+++ utils/checking.py
@@ -1,4 +1,6 @@
 """Методы для проверки ответов наших запросов"""
+import json
+
 from requests import Response
 
 
@@ -12,3 +14,10 @@ class Checking():
             print(f'Успешно!!! Статус код = {response.status_code}')
         else:
             print(f'Провал!!! Статус код = {response.status_code}')
+
+    """Метод для проверки наличия обязательных полей в ответе запроса"""
+    @staticmethod
+    def check_json_token(response: Response, expected_value):
+        token = json.loads(response.text)
+        assert list(token) == expected_value
+        print("Все поля присутствуют")
